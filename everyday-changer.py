4f4df251ab295d876f282cf43350bcf61ff7f4c5
svvashishtha <saurabh.vashisht@zomato.com>
2019-11-13 16:35:50 +0530

    change secondary desktop wallpaper too

--- everyday-changer.py
+++ everyday-changer.py
@@ -13,6 +13,18 @@ def set_background(filePath):
 	"""
 	return subprocess.Popen(script%filePath , shell = True)
 
+def  set_background_1(filePath1, filePath2):
+	filePath1 = "$HOME" + filePath1
+	filePath2 = "$HOME" + filePath2
+	script = """/usr/bin/osascript<<END
+	tell application "System Events"
+	set t to a reference to every desktop
+	set picture of item 1 of t to POSIX file "%s" -- display 1
+	set picture of item 2 of t to POSIX file "%s" -- display 2
+	end tell"""
+	print (script%(filePath1,filePath2))
+	return subprocess.Popen(script%(filePath1,filePath2) , shell = True)
+
 
 DATE_FILE = "last-updated.conf"
 DATE_FORMAT = "%Y/%m/%d %H:%M:%S"
@@ -30,8 +42,20 @@ if current_date.date() > last_date.date():
 	feed = feedparser.parse('https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss')
 	item = feed.entries[0]
 	imgUrl = item.links[1].href
-	subprocess.run(["./wallpaper.sh", imgUrl], cwd = path.abspath(path.dirname(__file__)))
-	result = set_background("$HOME/Pictures/random.png")
-	if not result.returncode:
-		with open(date_file_path, "w") as f:
-			f.write(current_date.strftime(DATE_FORMAT))
+
+	filename1 = imgUrl.rpartition("/")[-1]
+	filepath1  = "/Pictures/" + filename1 
+
+	subprocess.run(["./wallpaper.sh", imgUrl ,filepath1], cwd = path.abspath(path.dirname(__file__)))
+	
+	item = feed.entries[1]
+	imgUrl = item.links[1].href
+	filename2 = imgUrl.rpartition("/")[-1]
+	filepath2  = "/Pictures/" + filename2 
+	subprocess.run(["./wallpaper.sh", imgUrl ,filepath2], cwd = path.abspath(path.dirname(__file__)))
+	result = set_background_1(filepath1, filepath2)
+
+
+	# if not result.returncode:
+	# 	with open(date_file_path, "w") as f:
+	# 		f.write(current_date.strftime(DATE_FORMAT))
--- wallpaper.sh
+++ wallpaper.sh
@@ -3,14 +3,18 @@
 set -e
 cd `dirname $0`
 
-FILE_PATH="$HOME/Pictures/random.png"
 
-if [ $# -ne 1 ]; then
-    echo "usage: wallpaper.sh <url>"
+
+if [ $# -ne 2 ]; then
+    echo "usage: wallpaper.sh <url> <filename>"
     exit 1
 fi
 
 url=$1
-curl -L -o $FILE_PATH $url
-echo $FILE_PATH
+name=$2
+
+filename="$HOME${name}"
+echo $filename
+# filename="$HOME/Pictures/random.jpg"
+curl -L -o $filename $url
 
